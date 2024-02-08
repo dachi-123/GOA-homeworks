3af30ead8c5cb733e6fea7f5dea7974ebf54ce23
nrdachi2011@gmail.com <mrdachi2011@gmail.com>
2024-02-08 13:01:17 +0400

    second commit

--- day 1/lesson1.py
+++ day 1/lesson1.py
@@ -1,92 +1,25 @@
-from turtle import *
+name = "დაჩი"
+
+hello = "გამარჯობა,"
+info = "მე ვარ"
+howoldishe = "წლის,"
+whatshesheight = "ჩემი სიმაღლეა"
+exactheight = "სანტიმეტრი"
+aboutmyteacher = " და ჩემი რაზმის ლიდერი არის ძალიან კარგი ადამიანი."
 
 
-#we want to paint a house
+surname = "თავაძე"
 
+print(name)
 
+age = 12
+height = 159.5
 
-print("Dachi Tavadze")
-#step 1:  draw a square
+is_goodatmath = True
 
-speed(30)
-width(5)
-color("red")
-forward(200)
-left(90) 
+is_dontknowenglish = False
 
-forward(200)
-left(90)
-
-forward(200)
-left(90)
-
-forward(200)
-left(90)
-
-#end of squere
- 
- #drawing a door
-
-forward(70)
-color("green")
-begin_fill()
-left(90)
-forward(100)    #height of the door
-right(90)
-forward(50)
-right(90)
-forward(100)
-end_fill()
-
-penup()
-goto(200, 200)
-pendown()
-
-color("yellow")
-begin_fill()
-right(150)
-forward(200)
-left(120)
-forward(200)
-end_fill()
-
-#drawing windows
-
-left(30)
-color("red")
-forward(70)
-left(90)
-
-begin_fill()
-color("red")
-
-penup()
-pendown()
-forward(50)
-
-left(90)
-forward(50)
-right(-90)
-forward(50)
-end_fill()
-
-penup()
-right(180)
-forward(150)
-
-begin_fill()
-
-pendown()
-forward(50)
-right(90)
-
-forward(50)
-color("red")
-left(-90)
-forward(50)
-right(90)
-forward(50)
-end_fill()
+print(hello + " " + info + " " + name + " " + surname + " " + str(age) + " " + howoldishe + " " + whatshesheight + " " + str(height) + " " + exactheight + " " + aboutmyteacher)
 
 
 
@@ -95,4 +28,12 @@ end_fill()
 
 
 
-exitonclick()
+
+
+
+
+
+
+
+
+
