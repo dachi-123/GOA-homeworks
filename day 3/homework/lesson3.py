d5cc91763c41f9680ad5459d7e848b791ce405fd
nrdachi2011@gmail.com <mrdachi2011@gmail.com>
2024-02-12 13:26:09 +0400

    day4

---
+++ day 3/homework/lesson3.py
@@ -0,0 +1,12 @@
+My_sister ="ლიზა ჩემი დაიკო,"
+Me ="დაჩი ანუ მეთვითონ,"
+Father ="თორნიკე მამაჩემი,"
+Mother ="რუსკა დედაჩემი,"
+Grandfather ="ანზორი ბაბუაჩემი"
+Auxiliary_word ="და"
+Grandmother ="ნანა ბებიაჩემი."
+A_Brief_Introduction ="ჩემი ოჯახის წევრების სახელები ძალიან მრავალფეროვანია. ესენია:"
+
+print(A_Brief_Introduction + " " + My_sister + " " + Me + " " + Father + " " + Mother + " " + Grandfather + " " + Auxiliary_word + " " + Grandmother)
+
+
