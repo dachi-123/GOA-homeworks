7101a9b9076cbf357b629f280e84bfe000740fff
nrdachi2011@gmail.com <mrdachi2011@gmail.com>
2024-02-18 10:22:18 +0400

    day4

---
+++ lesson4.py
@@ -0,0 +1,9 @@
+intrudaction = "me var "
+name = "dachi"
+surname = "tavadze"
+my_age = 12 
+continue1 = "magram 10 wlis shemdeg viqnebi"
+my_age =my_age + 10
+age = "wlis"
+print(intrudaction + " " + name + " " + surname + " " + continue1 + " "+ str(my_age) + " " + age )
+
