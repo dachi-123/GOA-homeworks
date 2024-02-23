7f0010b0ef2a64c18d0936e26e8fce422adc684b
nrdachi2011@gmail.com <mrdachi2011@gmail.com>
2024-02-23 15:32:55 +0400

    lesson5

---
+++ day5/homework5.py
@@ -0,0 +1,44 @@
+#introduction my family members ages
+
+my_mum_age = 32
+my_dad_age = 33
+my_sister_age = 8
+my_age = 12
+my_grandfather_age = 62
+my_grandmother_age = 61
+
+#variables
+my_mum_age = my_mum_age + 5 
+my_dad_age = my_dad_age + 5 
+my_sister_age = my_sister_age + 5
+my_age = my_age + 5
+my_grandfather_age = my_grandfather_age + 5
+my_grandmother_age = my_grandmother_age + 5
+
+#str introduction
+nothing_special = "chemi ojaxis wevrebis asaki 5 wlis shemdeg iqneba:- "
+mother = "dedachemi "
+father = ", mamachemi "
+sister = ", daiko "
+myself = ", me "
+grandfather = ", babuachemi "
+grandmother = ", bebiachemi "
+end_of_variables = ".eseti iqneba chemi ojaxis wevrebis wlovaneba 5 wlis shemdeg "
+
+#Age of family members after 5 years by variables
+print(nothing_special + mother + str(my_mum_age) + father + str(my_dad_age) + sister + str(my_sister_age) + myself + str(my_age) + grandfather + str(my_grandfather_age) + grandmother + str(my_grandmother_age ) + end_of_variables)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
